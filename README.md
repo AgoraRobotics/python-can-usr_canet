7806eea30b300155d994a22cb71209c6a1e06123
Viorel Stirbu <vio@agorarobotics.com>
2024-03-19 15:16:57 +0200

    first version of USR-CANET200 python-can driver

---
+++ README.md
@@ -0,0 +1,24 @@
+## python-can-usr_canet
+
+
+
+This module is a plugin that lets you use [USR-CANET200 adapters](https://www.pusr.com/products/can-to-ethernet-converters-usr-canet200.html) in python-can.
+
+
+### Installation
+
+Install using pip:
+
+    $ pip install python-can-usr_canet
+
+
+### Usage
+
+Overall, using this plugin is quite similar to the main Python-CAN library, with the interface named `usr_canet`. For most scenarios, incorporating a USR-CANET200 interface is as easy as modifying Python-CAN examples with the lines provided below:
+
+Create python-can bus with the CANET200 ETH interface:
+
+    import can
+    from usr_canet import UsrCanetBus
+
+    bus = can.Bus(interface="usr_canet", bitrate=250000)
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+setup.py
+
+python-can-usr_canet
+"""
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="python-can-usr_canet",
+    version="0.1.0",
+    author="Viorel Stirbu",
+    author_email="vio@agorarobotics.com",
+    description="Python-can USR-CANET200",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AgoraRobotics/python-can-usr_canet",
+    py_modules = ["usr_canet"],
+    python_requires=">=3.9",
+    install_requires=[
+        "python-can",
+    ],
+    entry_points = {
+        'can.interface': [
+            'usr_canet = usr_canet:UsrCanetBus'
+        ]
+    }
+)
