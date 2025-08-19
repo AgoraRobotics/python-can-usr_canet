8a4fdc00001c14cb8a84df3418c3fa36c4034148
Viorel Stirbu <viorels@gmail.com>
2025-08-19 19:56:54 +0300

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="python-can-usr_canet",
-    version="0.2.0",
+    version="0.3.0",
     author="Viorel Stirbu",
     author_email="vio@agorarobotics.com",
     description="Python-can USR-CANET200",
